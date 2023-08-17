db192ae8eefe7b641242d83855d826cb9927bc66
robeertosv <resvolkava@gmail.com>
2023-08-17 14:04:44 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const port = 3000
+
+app.use(express.json())
+app.use(express.static('./view'))
+
+let tasks = [] //Tareas pendientes
+let completed = [] //Tareas completadas
+let newId = 0; //ID de la tarea
+
+server.listen(port, () => console.log(`VIEW APP IN: http://localhost:${port}`))  //Arrancar server en el puerto almacenado en la variable port
+
+//Mostrar index.html y sus archivos asociados al acceder a la url /
+app.get('/', (req, res) => {
+    res.send('./view')
+})
+
+//Devuelve un JSON con la id de la tarea más nueva
+app.get('/api/tasks/id', (req, res) => {
+    res.json(newId)
+})
+
+//Devuelve un JSON con todas las tareas pendientes al acceder a /api/tasks con GET
+app.get('/api/tasks', (req, res) => {
+    res.json(tasks)
+})
+
+//Crea una nueva tarea al acceder a /api/tasks con POST
+app.post('/api/tasks', (req, res) => {
+    let newT = {
+        id: newId,
+        title: req.body.title,
+        desc: req.body.desc,
+        completed: false
+    }
+    tasks.push(newT)
+    newId++
+
+    res.status(201).json(newT)
+})
+
+//Devuelve un JSON con las tareas completadas al acceder a /api/complete con GET
+app.get('/api/complete', (req, res) => {
+    res.json(completed)
+})
+
+//Elimina la tarea por id al acceder a /api/delete con POST
+app.post('/api/delete', (req, res) => {
+
+    let idToDelete = req.body.id;
+
+    tasks = tasks.filter(obj => obj.id !== idToDelete);
+
+    res.status(202).json(tasks)
+})
+
+//Añade a la lista de tareas completadas la tarea con el id requerido usando el método POST
+app.post('/api/complete', (req, res) => {
+    /*let elem = req.body.id
+    let a = tasks.splice(elem, 1);
+    completed.push(a)
+    newId--
+    res.status(202).json(completed)*/
+
+    let idToDelete = req.body.id;
+
+
+    tasks = tasks.filter(obj => {
+        if (obj.id === idToDelete) {
+            completed.push(obj); // Hacer push del objeto al array de objetos borrados
+            return false; // No incluir este objeto en el nuevo array
+        }
+        return true; // Incluir este objeto en el nuevo array
+    });
+    res.status(202).json(completed)
+
+})
---
+++ view/main.js
@@ -0,0 +1,93 @@
+const pending = document.querySelector('.pending');
+const completedDiv = document.querySelector('.completed');
+
+async function getTasks() {
+    let res = await fetch('http://localhost:3000/api/tasks');
+    res = await res.json();
+
+    res.forEach((task) => {
+        pending.innerHTML += `
+        <div class="element">
+            <h1>${task.title}</h1>
+            <p>${task.desc}</p>
+            <button onclick="completedT(${task.id})">Complete</button>
+            <button id="elim" onclick="erase(${task.id})">Erase</button>
+        </div>
+    `
+    })
+}
+
+getTasks()
+
+async function getCompleted() {
+    let res = await fetch('http://localhost:3000/api/complete');
+    res = await res.json();
+    res.forEach((item) => {
+        completedDiv.innerHTML += `
+        <div class="element">
+            <h1>${item.title}</h1>
+            <p>${item.desc}</p>
+        </div>
+    `
+    })
+}
+
+getCompleted()
+
+async function createTask(e) {
+
+    let title = document.querySelector('#title').value
+    let desc = document.querySelector('#desc').value
+
+    if (title == "undefined" || desc == "undefined") { alert("ERROR, uno de los valores ha sido undefined"); return }
+    let newT = {
+        title: title,
+        desc: desc
+    }
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(newT);
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    let req = await fetch('http://localhost:3000/api/tasks', requestOptions)
+}
+
+function erase(tid) {
+    let newT = {
+        id: tid
+    }
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(newT);
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    let res = fetch('http://localhost:3000/api/delete', requestOptions).then(() => window.location.reload())
+}
+
+function completedT(tid) {
+    let newT = {
+        id: tid
+    }
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(newT);
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    let res = fetch('http://localhost:3000/api/complete', requestOptions).then(() => window.location.reload())
+}
